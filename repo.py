dc9f64955b46d0e5a66ae18098c1b6088c00390a
HM <hieumdd@gmail.com>
2022-07-19 21:29:17 +0700

    fix: Fix token

--- repo.py
+++ repo.py
@@ -7,7 +7,7 @@ import httpx
 def get() -> list[dict[str, Any]]:
     r = httpx.get(
         "https://api.elvesapp.com/api/utils/analytics/requests",
-        headers={"Authorization": os.getenv("TOKEN", "")},
+        headers={"Authorization": os.getenv("API_TOKEN", "")},
     )
     data = r.json()
     return data
